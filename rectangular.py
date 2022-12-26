35776ebef51e2aa43979a3454b57995ffc90b927
generali.collins <104697851+generali-collins@users.noreply.github.com>
2022-12-26 10:10:14 +0000

    Add files via upload
    Drawing a rectangle using the turtle module method

---
+++ rectangular.py
@@ -0,0 +1,12 @@
+import turtle
+def drawRectangle(myTurtle,width,height):
+    myTurtle.forward(width)
+    myTurtle.right(90)
+    myTurtle.forward(height)
+    myTurtle.right(90)
+    myTurtle.forward(width)
+    myTurtle.right(90)
+    myTurtle.forward(height)
+    myTurtle.right(90)
+    
+    
